c325ad13994d146caa901b1cdac6bee28eeb89d8
Boris Veselov <borisveselov@yahoo.com>
2022-04-21 16:23:44 -0400

    updated gitignore and tables

---
+++ db/db.sql
@@ -0,0 +1,4 @@
+DROP DATABASE IF EXISTS tracking;
+
+CREATE DATABASE tracking;
+USE tracking;
---
+++ db/seeds.sql
@@ -0,0 +1,13 @@
+USE tracking;
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES
+  ('Bob', 'Bibbins', 1, 5),
+  ('Rob', 'Robbins', 2, 5),
+  ('Todd', 'Toddins', 1, 5),
+  ('Fred', 'Freddins', 2, 5),
+  ('Greg', 'Greggins', 1, 5),
+  ('Tedd', 'Teddins', 2, 10),
+  ('Redd', 'Reddins', 1, 10),
+  ('Bill', 'Billers', 2, 10),
+  ('Nill', 'Nillers', 1, 10),
+  ('Phill', 'Phillers', 2, 10);
